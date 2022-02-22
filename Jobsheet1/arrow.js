9f8dd3c1424dd9f25023073da48c98bc2d9eb6d3
brilliandyzufar <brilliandy12@gmail.com>
2022-02-22 22:02:38 +0700

    Jobsheet 1
    Signed-off-by: brilliandyzufar <brilliandy12@gmail.com>

---
+++ Jobsheet1/arrow.js
@@ -0,0 +1,9 @@
+// Tanpa arrow
+// funciton gretting(message){
+// return alert(`${message} Cantik !`);
+//}
+
+//Menggunakan arrow
+let gretting = message => alert(`${message} Cantik !`);
+
+gretting('Selamat Pagi');
