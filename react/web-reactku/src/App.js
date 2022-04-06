3fe0fce803c877d7c52ea63f604345dcbea7f119
brilliandyzufar <brilliandy12@gmail.com>
2022-04-06 19:55:43 +0700

    Higher Order Components
    Signed-off-by: brilliandyzufar <brilliandy12@gmail.com>

--- react/web-reactku/src/App.js
+++ react/web-reactku/src/App.js
@@ -153,151 +153,168 @@
 //     </Router>
 //   );
 // }
-// // NOMOR 4
-import React, { useContext, createContext, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useLocation,
-  useNavigate,
-  Navigate,
-  Outlet
-} from "react-router-dom";
+// // JS 6
+// import React, { useContext, createContext, useState } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link,
+//   useLocation,
+//   useNavigate,
+//   Navigate,
+//   Outlet
+// } from "react-router-dom";
 
-export default function AuthExample() {
-  return (
-    <ProvideAuth>
-      <Router>
-        <div>
-          <AuthButton />
+// export default function AuthExample() {
+//   return (
+//     <ProvideAuth>
+//       <Router>
+//         <div>
+//           <AuthButton />
 
-          <ul>
-            <li>
-              <Link to="/public">Public Page</Link>
-            </li>
-            <li>
-              <Link to="/protected">Protected Page</Link>
-            </li>
-          </ul>
+//           <ul>
+//             <li>
+//               <Link to="/public">Public Page</Link>
+//             </li>
+//             <li>
+//               <Link to="/protected">Protected Page</Link>
+//             </li>
+//           </ul>
 
-          <Routes>
-            <Route path="/public" element={<PublicPage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route element={<PrivateRoute/>}>
-              <Route path="/protected" element={<ProtectedPage/>}/>
-            </Route>
-          </Routes>
-        </div>
-      </Router>
-    </ProvideAuth>
-  );
-}
+//           <Routes>
+//             <Route path="/public" element={<PublicPage/>}/>
+//             <Route path="/login" element={<LoginPage/>}/>
+//             <Route element={<PrivateRoute/>}>
+//               <Route path="/protected" element={<ProtectedPage/>}/>
+//             </Route>
+//           </Routes>
+//         </div>
+//       </Router>
+//     </ProvideAuth>
+//   );
+// }
 
-const fakeAuth = {
-  isAuthenticated: false,
-  signin(cb) {
-    fakeAuth.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    fakeAuth.isAuthenticated = false;
-    setTimeout(cb, 100);
-  }
-};
+// const fakeAuth = {
+//   isAuthenticated: false,
+//   signin(cb) {
+//     fakeAuth.isAuthenticated = true;
+//     setTimeout(cb, 100); // fake async
+//   },
+//   signout(cb) {
+//     fakeAuth.isAuthenticated = false;
+//     setTimeout(cb, 100);
+//   }
+// };
 
-const authContext = createContext();
+// const authContext = createContext();
 
-function ProvideAuth({ children }) {
-  const auth = useProvideAuth();
-  return (
-    <authContext.Provider value={auth}>
-      {children}
-    </authContext.Provider>
-  );
-}
+// function ProvideAuth({ children }) {
+//   const auth = useProvideAuth();
+//   return (
+//     <authContext.Provider value={auth}>
+//       {children}
+//     </authContext.Provider>
+//   );
+// }
 
-function useAuth() {
-  return useContext(authContext);
-}
+// function useAuth() {
+//   return useContext(authContext);
+// }
 
-function useProvideAuth() {
-  const [user, setUser] = useState(null);
+// function useProvideAuth() {
+//   const [user, setUser] = useState(null);
 
-  const signin = cb => {
-    return fakeAuth.signin(() => {
-      setUser("user");
-      cb();
-    });
-  };
+//   const signin = cb => {
+//     return fakeAuth.signin(() => {
+//       setUser("user");
+//       cb();
+//     });
+//   };
 
-  const signout = cb => {
-    return fakeAuth.signout(() => {
-      setUser(null);
-      cb();
-    });
-  };
+//   const signout = cb => {
+//     return fakeAuth.signout(() => {
+//       setUser(null);
+//       cb();
+//     });
+//   };
 
-  return {
-    user,
-    signin,
-    signout
-  };
-}
+//   return {
+//     user,
+//     signin,
+//     signout
+//   };
+// }
 
-function AuthButton() {
-  let navigate = useNavigate();
-  let auth = useAuth();
+// function AuthButton() {
+//   let navigate = useNavigate();
+//   let auth = useAuth();
 
-  return auth.user ? (
-    <p>
-      Welcome!{" "}
-      <button
-        onClick={() => {
-          auth.signout(() => navigate("/"));
-        }}
-      >
-        Sign out
-      </button>
-    </p>
-  ) : (
-    <p>You are not logged in.</p>
-  );
-}
+//   return auth.user ? (
+//     <p>
+//       Welcome!{" "}
+//       <button
+//         onClick={() => {
+//           auth.signout(() => navigate("/"));
+//         }}
+//       >
+//         Sign out
+//       </button>
+//     </p>
+//   ) : (
+//     <p>You are not logged in.</p>
+//   );
+// }
 
-function PrivateRoute({ children, ...rest}){
-  let auth = useAuth();
-  return auth.user ? <Outlet /> : <Navigate to="/login" />;
-}
+// function PrivateRoute({ children, ...rest}){
+//   let auth = useAuth();
+//   return auth.user ? <Outlet /> : <Navigate to="/login" />;
+// }
 
-function PublicPage() {
-  return <h3>Public</h3>;
-}
+// function PublicPage() {
+//   return <h3>Public</h3>;
+// }
 
-function ProtectedPage() {
-  return(
+// function ProtectedPage() {
+//   return(
     
-    <h3>Protected</h3>
-  );
+//     <h3>Protected</h3>
+//   );
+// }
+
+// function LoginPage() {
+//   let navigate = useNavigate();
+//   let location = useLocation();
+//   let auth = useAuth();
+
+//   let { from } = location.state || { from: { pathname: "/protected" } };
+//   let login = () => {
+//     auth.signin(() => {
+//       navigate("/", {replace: true});
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <p>You must log in to view the page at {from.pathname}</p>
+//       <button onClick={login}>Log in</button>
+//     </div>
+//   );
+// }
+import React, { Component } from "react";
+import './App.css'
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
+
+class App extends Component {
+    render(){
+        return(
+            <div className="App">
+                <ClickCounter />
+                <HoverCounter />
+                </div>
+        )
+    }
 }
 
-function LoginPage() {
-  let navigate = useNavigate();
-  let location = useLocation();
-  let auth = useAuth();
-
-  let { from } = location.state || { from: { pathname: "/protected" } };
-  let login = () => {
-    auth.signin(() => {
-      navigate("/", {replace: true});
-    });
-  };
-
-  return (
-    <div>
-      <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
-    </div>
-  );
-}
+export default App
