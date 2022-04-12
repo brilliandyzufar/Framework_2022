645e879fce60982703eec536222196c6928bf84a
brilliandyzufar <brilliandy12@gmail.com>
2022-04-12 17:25:24 +0700

    UI Design & API
    Signed-off-by: brilliandyzufar <brilliandy12@gmail.com>

---
+++ uts/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+import Users from './Users';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Users />
+    </div>
+  );
+}
+
+export default App;
