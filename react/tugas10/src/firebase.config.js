eec38382584c57079d20b707f3f607bb35fb34a9
brilliandyzufar <brilliandy12@gmail.com>
2022-05-25 16:23:14 +0700

    Firebase
    Signed-off-by: brilliandyzufar <brilliandy12@gmail.com>

---
+++ react/tugas10/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyA8CdI8HrMgyP-qaBFoJrsW7corFpSEOPs",
+    authDomain: "jobsheet10-f6fe7.firebaseapp.com",
+    projectId: "jobsheet10-f6fe7",
+    storageBucket: "jobsheet10-f6fe7.appspot.com",
+    messagingSenderId: "31954274756",
+    appId: "1:31954274756:web:9b9c07ea2afed27945bc91",
+    measurementId: "G-XVFVMP771S"
+};
+
